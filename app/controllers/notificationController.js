8be17df8e114ff79c7826eedf61179878067cd2a
Abdelhadi <abdo.sabani30@gmail.com>
2021-09-14 22:57:14 +0100

    Add: get notifications functionality

--- app/controllers/notificationController.js
+++ app/controllers/notificationController.js
@@ -1,7 +1,8 @@
 const { Notification } = require("../models/notificationModel");
 const { isUserActive, getSessions } = require("../utils/redis");
+const { chatValidator } = require("../utils/validationSchema");
 const socket = require("../utils/socketIo");
-// const createError = require("http-errors");
+const createError = require("http-errors");
 
 const addNotification = async (payload) => {
     try {
@@ -27,6 +28,69 @@ const addNotification = async (payload) => {
     }
 };
 
+const getNotifications = async (req, res, next) => {
+    try {
+        let currentUser = req.currentUser;
+        let perPage = 10;
+        let query = await chatValidator(req.query, { createdAt: 2 });
+        let matchQuery = {};
+        if (query.createdAt) {
+            matchQuery = {
+                $and: [
+                    {
+                        createdAt: { $lt: query.createdAt },
+                    },
+                    {
+                        receiver: currentUser._id,
+                    },
+                ],
+            };
+        } else {
+            matchQuery = {
+                receiver: currentUser._id,
+            };
+        }
+        let notifications = await Notification.aggregate([
+            { $match: matchQuery },
+            {
+                $lookup: {
+                    from: "users",
+                    let: { userId: "$sender" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $eq: ["$_id", "$$userId"],
+                                },
+                            },
+                        },
+                        {
+                            $project: {
+                                userName: 1,
+                                profile: 1,
+                            },
+                        },
+                    ],
+                    as: "sender",
+                },
+            },
+            {
+                $unwind: "$sender",
+            },
+            {
+                $sort: {
+                    createdAt: -1,
+                },
+            },
+            { $limit: perPage },
+        ]);
+        res.json(notifications);
+    } catch (err) {
+        next(err);
+    }
+};
+
 module.exports = {
     addNotification,
+    getNotifications,
 };
